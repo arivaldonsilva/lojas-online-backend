b634f6abeb047017790953894f3e4909fc4fa25f
Arivaldo do Nascimento Silva <arivaldon.silva@gmail.com>
2018-09-03 21:02:46 -0300

    Criando repository e service para Categoria

--- src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/CategoriaResource.java
@@ -3,23 +3,34 @@ package com.nelioalves.cursomc.resources;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id).orElse(null);
 		
-		Categoria c1 = new Categoria(1, "Informática");
-		Categoria c2 = new Categoria(2, "Escritório");
-		List<Categoria> categorias = new ArrayList<>(Arrays.asList(c1, c2));
-		return categorias;
+		return ResponseEntity.ok(obj);
+		//Categoria c1 = new Categoria(1, "Informática");
+		//Categoria c2 = new Categoria(2, "Escritório");
+		//List<Categoria> categorias = new ArrayList<>(Arrays.asList(c1, c2));
+		//return categorias;
 	}
 }
